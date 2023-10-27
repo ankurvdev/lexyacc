21c77cd6a283047f5add015653463ba99c1f4cd5
Ankur <ankurv@verma>
2023-10-26 22:59:50 -0700

    Sync fixes

--- ci/vcpkg-additional-ports/ankurvdev-lexyacc/portfile.cmake
+++ ci/vcpkg-additional-ports/ankurvdev-lexyacc/portfile.cmake
@@ -17,12 +17,14 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 if(HOST_TRIPLET STREQUAL TARGET_TRIPLET) # Otherwise fails on wasm32-emscripten
     vcpkg_copy_tools(TOOL_NAMES lexyacc AUTO_CLEAN)
+else()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
 file(READ "${CURRENT_PACKAGES_DIR}/share/lexyacc/LexYaccConfig.cmake" config_contents)
 file(WRITE "${CURRENT_PACKAGES_DIR}/share/lexyacc/LexYaccConfig.cmake"
 "find_program(
-    LEXYACC_EXECUTABLE lexyacc
+    lexyacc_EXECUTABLE lexyacc
     PATHS
         \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/${PORT}\"
     NO_DEFAULT_PATH
